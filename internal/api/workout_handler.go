4e8344baa7e524249dc475220fe066788c3fac17
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2025-03-18 16:42:13 -0700

    Feat: Created the handlers for workout

---
+++ internal/api/workout_handler.go
@@ -0,0 +1,37 @@
+package api
+
+import (
+	"fmt"
+	"github.com/go-chi/chi/v5"
+	"net/http"
+	"strconv"
+)
+
+type WorkoutHandler struct{}
+
+func NewWorkoutHandler() *WorkoutHandler {
+	return &WorkoutHandler{}
+}
+
+// Get the workout
+func (wh *WorkoutHandler) HandlerGetWorkoutByID(w http.ResponseWriter, r *http.Request) {
+	paramsWorkoutId := chi.URLParam(r, "id")
+
+	if paramsWorkoutId == "" {
+		http.NotFound(w, r)
+		return
+	}
+
+	workoutId, err := strconv.ParseInt(paramsWorkoutId, 10, 64)
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "this is the workout id %d\n", workoutId)
+}
+
+// Create a workout
+func (wh *WorkoutHandler) HandlerCreateWorkout(w http.ResponseWriter, r *http.Request) {
+
+}
